4354a67062c6dffe64994cfbf199497e172e95a7
sumit567 <sumit567@gmail.com>
2016-09-17 10:08:17 -0400

    Facebook login added

--- app/src/main/java/com/kunalkhamar/watvocab_android/LoginActivity.java
+++ app/src/main/java/com/kunalkhamar/watvocab_android/LoginActivity.java
@@ -2,12 +2,23 @@ package com.kunalkhamar.watvocab_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
         setContentView(R.layout.activity_login);
     }
 }
